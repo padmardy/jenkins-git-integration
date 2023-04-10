82ba50ede940d03539e398819e0a06285987059d
padmardy <padmasurakanti09@gmail.com>
2023-04-10 18:31:27 +0530

    targets

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ scp "jenkins-integration.tar.gz" ubuntu@<IP address of tomcat>:/home/ubuntu/
 	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>copying tar build done<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 	ssh ubuntu@<9000> "
 	  tar -xvf jenkins-integration.tar.gz
-	" 
-
-scp "${REPO}-$ENV.tar.gz" ubuntu@<>:/home/ubuntu/tomcat/webapps
-	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>copying tar build done<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-	ssh ubuntu@<ip> "
-	  cd target			  
+      rm -rf *.tar.tz
+      sudo systemctl stop tomcat.service
+      rm -rf /home/ubuntu/tomcat/webapps/*.war
+      cp -r jenkins-intergration.war /home/ubuntu/tomcat/webapps/
+      sudo systemctl start tomcat.service
 	"
